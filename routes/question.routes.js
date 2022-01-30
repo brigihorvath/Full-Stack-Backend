4e83d1777789919d14d02dbb431cbbea5fc7caac
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-30 19:08:31 +0100

    add get main test route

--- routes/question.routes.js
+++ routes/question.routes.js
@@ -6,25 +6,33 @@ const axios = require('axios');
 // CRUD - Create
 router.post(
   '/questions/create',
-  // fileUploader.single('tour-cover-image'),
-  async (req, res) => {
-    try {
-      const question = req.body;
-      console.log(req.body);
-
-      const newQuestion = await Question.create({
-        question,
-      });
-      // console.log(newTour);
-      res.status(201).json({
-        status: 'success',
-        data: {
-          content: newQuestion,
-        },
-      });
-    } catch (err) {
-      console.log(err);
-    }
+  (req, res) => {
+    console.log(req.body);
   }
+  // fileUploader.single('tour-cover-image'),
+  //   async (req, res) => {
+  //     try {
+  //       const question = req.body;
+  //       console.log(req.body);
+
+  //       const newQuestion = await Question.create({
+  //         question,
+  //       });
+  //       // console.log(newTour);
+  //       res.status(201).json({
+  //         status: 'success',
+  //         data: {
+  //           content: newQuestion,
+  //         },
+  //       });
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
 );
+
+//README.
+router.get('/', (req, res) => {
+  console.log('get here');
+});
 module.exports = router;
